9bd187104b7fcdb43ec32b4e7762eee129af6ebc
Chloe V <chloe.m.van@gmail.com>
2024-06-12 18:05:42 -0700

    fix dependency issues

--- vite-project/src/redux/store.jsx
+++ vite-project/src/redux/store.jsx
@@ -4,6 +4,8 @@ import membersReducer from '../features/members/membersSlice';
 // configureStore(configuration object)
 //    reducer property is an object that maps slice names to reducer functions.
 //    membersReducer manages state available under the members key (slice name)
+// Thunks requires that the redux-thunk middleware (a type of plugin for Redux) be added to the Redux store when it's created.
+// Fortunately configureStore function already sets that up for us automatically, so we can go ahead and use thunks here.
 const store = configureStore({
   reducer: {
     members: membersReducer,
